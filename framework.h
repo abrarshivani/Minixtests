0d54220864af5a764aae57af32f6f30d56e39a0d
Abrar Shivani <ashivani@hawk.iit.edu>
2017-03-31 17:16:06 -0700

    Add testing framework

---
+++ framework.h
@@ -0,0 +1,43 @@
+//
+// Created by Abrar Shivani on 2/26/17.
+//
+
+#ifndef FRAMEWORK_H
+#define FRAMEWORK_H
+
+
+/* module wide constants */
+#define PAGE_SIZE 4096
+
+/* return code definitions */
+typedef int RC;
+
+#define RC_OK 1
+
+/* holder for error messages */
+extern char *RC_message;
+
+/* print a message to standard out describing the error */
+extern void printError (RC error);
+extern char *errorMessage (RC error);
+
+#define THROW(rc,message) \
+  do {			  \
+    RC_message=message;	  \
+    return rc;		  \
+  } while (0)		  \
+
+// check the return code and exit if it is an error
+#define CHECK(code)							\
+  do {									\
+    int rc_internal = (code);						\
+    if (rc_internal != RC_OK)						\
+      {									\
+	char *message = errorMessage(rc_internal);			\
+	printf("[%s-L%i-%s] ERROR: Operation returned error: %s\n",__FILE__, __LINE__, __TIME__, message); \
+	free(message);							\
+	exit(1);							\
+      }									\
+  } while(0);
+
+#endif //FRAMEWORK_H
