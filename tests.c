34c0e62b2bfe920e7f28a8f54cb5ef7d6959f3db
Abrar Shivani <ashivani@hawk.iit.edu>
2017-03-31 18:24:55 -0700

    Add duplicate tests

--- tests.c
+++ tests.c
@@ -13,18 +13,41 @@ int block(char *topic_name, char *message);
 
 static int test_create_topic();
 static int test_block_publisher();
-/*
 static int test_create_duplicate_topic();
 static int test_add_publisher_twice();
 static int test_add_subscriber_twice();
 static int test_add_publisher_subscriber_unknown_topic();
 static int test_retrieve_unknown_topic();
 static int test_retrieve_different_topic();
-*/
+
+char error_msgs[] = {
+        "SUCCESS",
+        "error",
+        "Already Publisher",
+        "Already Subscriber",
+        "Not a publisher",
+        "No subscriber",
+        "No matching topic found",
+        "Max topic per process reached",
+        "err_msg_full",
+        "No new message available",
+        "Max topic reached",
+        "Topic exists",
+        "Error empty message",
+        "Max characters in topic name",
+        "No empty topic name",
+        "No handle found",
+};
 
 testcase_t testcases[] = {
         &test_create_topic,
         &test_block_publisher,
+        &test_create_duplicate_topic,
+//        &test_add_publisher_twice,
+//        &test_add_subscriber_twice,
+//        &test_add_publisher_subscriber_unknown_topic,
+//        &test_retrieve_unknown_topic,
+//        &test_retrieve_different_topic,
 };
 
 int
@@ -33,6 +56,8 @@ main()
     uint64_t ntestcases = 0;
     uint64_t totaltestcases = sizeof(testcases) / sizeof(testcase_t);
 
+    printf("Note: You need to run these tests \
+            before running other program which uses this topic system calls");
     for (ntestcases = 0; ntestcases < totaltestcases; ntestcases++) {
         (testcases[ntestcases])();
         printf("Executing tests: %s\n", testName);
@@ -40,19 +65,23 @@ main()
     return 0;
 }
 
-int
+static int
 test_create_topic()
 {
     char *topic_name = "test";
     status_t status = ok;
     testName = "Create new topic";
     status = create_topic(topic_name);
-    ASSERT_EQUALS_INT(status, ok, "Successfully created topic");
+    if (status == ok) {
+        printf("topic %s created successfully", topic_name);
+    } else {
+        printf("WARNING: You need to reboot to run tests", topic_name);
+    }
     TEST_DONE();
     return status;
 }
 
-int
+static int
 test_block_publisher()
 {
     uint64_t max_msgs = 10;
@@ -71,7 +100,7 @@ test_block_publisher()
     if (fork() == 0) {
         add_subscriber(topic_name);
         for (nmsg = 0; nmsg < max_msgs; nmsg++) {
-            sleep(3);
+            sleep(7);
             status = retrieve_message(topic_name, &rmsg);
             if (status == ok) {
                 printf("Message Retrieved: %d\n", nmsg);
@@ -95,3 +124,51 @@ int block(char *topic_name, char *message) {
         status = publish_message(topic_name, message);
     } while(status == err_msg_full);
 }
+
+static int
+test_create_duplicate_topic()
+{
+    char *topic_name = "duptest";
+    status_t status = ok;
+    testName = "Create topic with existing name";
+    status = create_topic(topic_name);
+    if (status == err_topic_exist) {
+        printf("WARNING: You need to reboot to run tests", topic_name);
+    }
+    status = create_topic(topic_name);
+    ASSERT_EQUALS_INT(status, err_topic_exist, error_msgs[err_topic_exist]);
+    TEST_DONE();
+    return status;
+}
+
+/*
+static int
+test_add_publisher_twice()
+{
+
+}
+
+static int
+test_add_subscriber_twice()
+{
+
+}
+
+static int
+test_add_publisher_subscriber_unknown_topic()
+{
+
+}
+
+static int
+test_retrieve_unknown_topic()
+{
+
+}
+
+static int
+test_retrieve_different_topic()
+{
+
+}
+*/
