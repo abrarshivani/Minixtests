94b7d324b1bdfe48b8422df02f4ddd0f0e62a09d
Abrar Shivani <ashivani@hawk.iit.edu>
2017-03-31 17:52:45 -0700

    Add blocking tests

--- tests.c
+++ tests.c
@@ -9,11 +9,22 @@
 
 char *testName;
 typedef int (*testcase_t)();
+int block(char *topic_name, char *message);
 
-int testCreateTopic();
+static int test_create_topic();
+static int test_block_publisher();
+/*
+static int test_create_duplicate_topic();
+static int test_add_publisher_twice();
+static int test_add_subscriber_twice();
+static int test_add_publisher_subscriber_unknown_topic();
+static int test_retrieve_unknown_topic();
+static int test_retrieve_different_topic();
+*/
 
 testcase_t testcases[] = {
-        &testCreateTopic,
+        &test_create_topic,
+        &test_block_publisher,
 };
 
 int
@@ -30,7 +41,7 @@ main()
 }
 
 int
-testCreateTopic()
+test_create_topic()
 {
     char *topic_name = "test";
     status_t status = ok;
@@ -41,3 +52,45 @@ testCreateTopic()
     return status;
 }
 
+int
+test_block_publisher()
+{
+    uint64_t max_msgs = 10;
+    uint64_t nmsg = 0;
+    int child_status = 0;
+    char *topic_name = "block";
+    char *msg = "msg";
+    char *rmsg = NULL;
+    testName = "Publisher blocks";
+
+    create_topic(topic_name);
+    add_subscriber(topic_name);
+    add_publisher(topic_name);
+
+    if (fork() == 0) {
+        add_subscriber(topic_name);
+        for (nmsg = 0; nmsg < max_msgs; nmsg++) {
+            sleep(2);
+            status = retrieve_msg(topic_name, &rmsg);
+            if (status == ok) {
+                printf("Message Retrieved: %s\n", nmsg);
+                free(rmsg);
+            }
+        }
+        exit(0);
+    } // child process
+
+    for (nmsg = 0; nmsg < max_msgs; nmsg++) {
+        printf("Publishing msg: %s\n", nmsg);
+        block(topic_name, msg);
+    }
+    wait(&child_status);
+}
+
+int block(char *topic_name, char *message) {
+    status_t status = ok;
+    do {
+        sleep(1);
+        status = publish_message(topic_name, message);
+    } while(status != err_msg_full);
+}
